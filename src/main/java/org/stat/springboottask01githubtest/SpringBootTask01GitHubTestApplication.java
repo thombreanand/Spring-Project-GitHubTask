9e42deb6bd33943c2196da81afe16ede410db7ea
anandthombre_45 <thombreanand101@gmail.com>
2026-03-19 23:17:21 +0530

    Initial commit

---
+++ src/main/java/org/stat/springboottask01githubtest/SpringBootTask01GitHubTestApplication.java
@@ -0,0 +1,15 @@
+package org.stat.springboottask01githubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTask01GitHubTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTask01GitHubTestApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/org/stat/springboottask01githubtest/SpringBootTask01GitHubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stat.springboottask01githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTask01GitHubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
